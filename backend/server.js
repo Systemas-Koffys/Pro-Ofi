88a4c9f4033cc89b1f7f40a232f854fdf11916d4
Kevin Flores <koffy61862629@gmail.com>
2026-05-11 21:51:36 -0400

    Actualizar server.js

--- backend/server.js
+++ backend/server.js
@@ -4,7 +4,7 @@ import mysql from 'mysql2/promise';
 import { exec } from 'child_process';
 
 const app = express();
-const port = 3000;
+const port = 5173;
 
 app.use(cors());
 app.use(express.json({ limit: '50mb' }));
